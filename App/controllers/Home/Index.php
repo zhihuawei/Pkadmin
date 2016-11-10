e4117c06eca2452d375602014c0fa078ef8d3dcb
h我叫小刀 <214564382@qq.com>
2016-11-10 18:12:05 +0800

    20161110 完成登录，首页等功能

---
+++ App/controllers/Home/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * ==========================================
+ * Created by Pocket Knife Technology.
+ * Author: ZhiHua_W <zhihua_wei@foxmail.com>
+ * Date: 2016/11/02 0031
+ * Time: 上午 9:10
+ * Project: Pkadmin后台管理系统
+ * Version: 1.0.0
+ * Power:  前台首页控制器
+ * ==========================================
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Index extends Home_Controller {
+
+	public function test() {
+		echo APPPATH . ADMIN_VIEW_DIR;
+		$this -> load -> view('welcome_message');
+	}
+
+}
