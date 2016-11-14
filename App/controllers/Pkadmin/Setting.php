5b127dd3f0e4eb98852b661d66d2e7e46b294609
h我叫小刀 <214564382@qq.com>
2016-11-14 08:46:02 +0800

    20161114新建网站设置控制器

---
+++ App/controllers/Pkadmin/Setting.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ==========================================
+ * Created by Pocket Knife Technology.
+ * Author: ZhiHua_W <zhihua_wei@foxmail.com>
+ * Date: 2016/11/14 0021
+ * Time: 上午 8:44
+ * Project: Pkadmin后台管理系统
+ * Version: 1.0.0
+ * Power: 网站设置控制器
+ * ==========================================
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Setting extends Pkadmin_Controller {
+	
+}
