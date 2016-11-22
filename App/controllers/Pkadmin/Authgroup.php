2c1aec919f8ca4a52eaa528292e73c5df82fc559
h我叫小刀 <214564382@qq.com>
2016-11-22 16:05:23 +0800

    2016-11-22 新建角色管理页面

---
+++ App/controllers/Pkadmin/Authgroup.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ==========================================
+ * Created by Pocket Knife Technology.
+ * Author: ZhiHua_W <zhihua_wei@foxmail.com>
+ * Date: 2016/11/22 0024
+ * Time: 下午 3:36
+ * Project: Pkadmin后台管理系统
+ * Version: 1.0.0
+ * Power: 后台权限组管理控制器
+ * ==========================================
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Authgroup extends Pkadmin_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this -> load -> library('pagination');
+		$this -> load -> model('admingroup_model', 'ag');
+	}
+
+	/**
+	 * 用户组管理首页
+	 */
+	public function index() {
+		
+	}
+
+}
