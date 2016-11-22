6bda379103b700aef23125425f4629f9652bd890
h我叫小刀 <214564382@qq.com>
2016-11-22 16:17:34 +0800

    2016-11-22

--- App/controllers/Pkadmin/Authgroup.php
+++ App/controllers/Pkadmin/Authgroup.php
@@ -26,7 +26,9 @@ class Authgroup extends Pkadmin_Controller {
 	 * 用户组管理首页
 	 */
 	public function index() {
-		
+		$data = $this -> data;
+		$data['auth_group'] = $this -> ag -> get_auth_group_list();
+		var_dump($data['auth_group']);
 	}
 
 }
