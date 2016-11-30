aaf0cf09ab5ca82f2616f781e62bf4e3e27817c4
h我叫小刀 <214564382@qq.com>
2016-11-30 16:37:26 +0800

    2016-11-30 新建文章分类控制器

---
+++ App/controllers/Pkadmin/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * ==========================================
+ * Created by Pocket Knife Technology.
+ * Author: ZhiHua_W <zhihua_wei@foxmail.com>
+ * Date: 2016/11/30 0025
+ * Time: 下午 4:33
+ * Project: Pkadmin后台管理系统
+ * Version: 1.0.0
+ * Power: 后台文章分类管理控制器
+ * ==========================================
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category extends Pkadmin_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this -> load -> library('pagination');
+		//		$this -> load -> model('article_model', 'ag');
+	}
+
+}
